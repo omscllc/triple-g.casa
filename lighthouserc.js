4c43bc5a5e9bbee0241b57e70a81daebcfab73c4
David Weiner <dave@triple-g.dev>
2025-10-10 17:57:15 -0400

    Update Lighthouse CI configuration to use GitHub App token and modify report storage settings

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,15 +4,10 @@ module.exports = {
     collect: {
       // The URL of your live staging site.
       url: ['https://stg.triple-g.dev'],
-      url: ['https://stg.triple-g.dev/portfolio'],
-      url: ['https://stg.triple-g.dev/contact'],
-      url: ['https://stg.triple-g.dev/about-me' ],
-      url: ['https://stg.triple-g.dev/gadgets'],
       numberOfRuns: 3,
     },
     upload: {
-      target: 'filesystem',
-      outputDir: './web/lhci_reports',
+      target: 'temporary-public-storage',
     },
   },
 };
